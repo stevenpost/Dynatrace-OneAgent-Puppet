b58e3717e1c2fc5066ddbd81923f9ed43b252891
Steven POST <steven.post@sfpd.fgov.be>
2025-05-19 11:51:54 +0200

    Fix linter issues

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,6 @@
 #   This class manages the installation of the OneAgent on the host
 #
 class dynatraceoneagent::install {
-
   $created_dir              = $dynatraceoneagent::created_dir
   $download_dir             = $dynatraceoneagent::download_dir
   $filename                 = $dynatraceoneagent::filename
@@ -14,34 +13,18 @@ class dynatraceoneagent::install {
   $package_state            = $dynatraceoneagent::package_state
   $oneagent_puppet_conf_dir = $dynatraceoneagent::oneagent_puppet_conf_dir
 
-  if ($::kernel == 'Linux' or $::osfamily  == 'AIX'){
-    exec { 'install_oneagent':
-        command   => $dynatraceoneagent::command,
-        cwd       => $download_dir,
-        timeout   => 6000,
-        creates   => $created_dir,
-        provider  => $provider,
-        logoutput => on_failure,
-    }
+  exec { 'install_oneagent':
+    command   => $dynatraceoneagent::command,
+    cwd       => $download_dir,
+    timeout   => 6000,
+    creates   => $created_dir,
+    provider  => $provider,
+    logoutput => on_failure,
   }
 
-  if ($::osfamily == 'Windows') {
-    package { $service_name:
-      ensure          => $package_state,
-      provider        => $provider,
-      source          => $download_path,
-      install_options => [$oneagent_params_hash, '--quiet'],
-    }
-  }
-
-  if ($reboot_system) and ($::osfamily == 'Windows') {
+  if ($reboot_system) {
     reboot { 'after':
-      subscribe => Package[$service_name],
+      subscribe => Exec['install_oneagent'],
     }
-  } elsif ($::kernel == 'Linux' or $::osfamily  == 'AIX') and ($reboot_system) {
-      reboot { 'after':
-        subscribe => Exec['install_oneagent'],
-      }
   }
-
 }
